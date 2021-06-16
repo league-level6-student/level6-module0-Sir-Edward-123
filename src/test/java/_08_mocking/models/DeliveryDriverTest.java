9279dbc855d185f6938a48c4fabc631eb4724a0e
11002084 <rzhu.mathleague@gmail.com>
2021-06-15 23:58:25 -0700

    6/15/2021
    Finished Delivery Driver Test and My Donut Shop Test

--- src/test/java/_08_mocking/models/DeliveryDriverTest.java
+++ src/test/java/_08_mocking/models/DeliveryDriverTest.java
@@ -12,37 +12,61 @@ import static org.mockito.Mockito.when;
 class DeliveryDriverTest {
 
     DeliveryDriver deliveryDriver;
+    
+    String name = "Bob";
+    
+    @Mock
+    Car car;
+    
+    @Mock
+    CellPhone cellPhone;
 
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	deliveryDriver = new DeliveryDriver(name, car, cellPhone);
     }
 
     @Test
     void itShouldWasteTime() {
         //given
+    	boolean expected = true;
+    	when(cellPhone.browseCatMemes()).thenReturn(true);
 
         //when
+    	boolean actual = deliveryDriver.wasteTime();
 
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldRefuel() {
         //given
+    	boolean expected = true;
+    	int octane = 90;
+    	when(car.fillTank(octane)).thenReturn(true);
 
         //when
+    	boolean actual = deliveryDriver.refuel(octane);
 
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldContactCustomer() {
         //given
+    	boolean expected = true;
+    	String phoneNum = "8581234567";
+    	when(cellPhone.call(phoneNum)).thenReturn(true);
 
         //when
+    	boolean actual = deliveryDriver.contactCustomer(phoneNum);
 
         //then
+    	assertEquals(expected, actual);
     }
 
 }
--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -16,37 +17,77 @@ import static org.mockito.Mockito.*;
 class MyDonutShopTest {
 
     MyDonutShop myDonutShop;
+    
+    @Mock
+    BakeryService bakeryService;
+    
+    @Mock
+    PaymentService paymentService;
+    
+    @Mock
+    DeliveryService deliveryService;
 
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
         //given
-
-        //when
-
+    	Order order = new Order(
+    			"Name",
+    			"Phone_Number",
+    			1, 
+    			2.50,
+    			"Credit_Card_Number",
+    			true);
+    	myDonutShop.openForTheDay();
+    	when(bakeryService.getDonutsRemaining()).thenReturn(1);
+    	
+        //when 	
         //then
+    	assertDoesNotThrow(() -> myDonutShop.takeOrder(order));
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
-
+    	Order order = new Order(
+    			"Name",
+    			"Phone_Number",
+    			5, 
+    			2.50,
+    			"Credit_Card_Number",
+    			true);
+    	myDonutShop.openForTheDay();
+    	when(bakeryService.getDonutsRemaining()).thenReturn(1);
+    	
         //when
-
+    	Throwable exceptionThrown = assertThrows(Exception.class, () -> myDonutShop.takeOrder(order));
+    	
         //then
+    	assertEquals("Insufficient donuts remaining", exceptionThrown.getMessage());
     }
 
     @Test
     void givenNotOpenForBusiness_whenTakeOrder_thenThrowIllegalStateException(){
         //given
+    	Order order = new Order(
+    			"Name",
+    			"Phone_Number",
+    			5, 
+    			2.50,
+    			"Credit_Card_Number",
+    			true);
+    	myDonutShop.closeForTheDay();
 
         //when
+    	Throwable exceptionThrown = assertThrows(Exception.class, () -> myDonutShop.takeOrder(order));
 
         //then
+    	assertEquals("Sorry we're currently closed", exceptionThrown.getMessage());
     }
 
 }

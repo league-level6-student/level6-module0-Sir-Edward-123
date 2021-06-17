c8def13cae8fba0e2cb4e3afbfbf88a45e259893
11002084 <rzhu.mathleague@gmail.com>
2021-06-17 15:20:36 -0700

    6/17/2021
    Finished News API Test and Astronauts Test

--- src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
+++ src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -26,28 +27,80 @@ import static org.mockito.Mockito.*;
 class NewsApiTest {
 
     NewsApi newsApi;
+    
+    @Mock
+    WebClient webClient;
+    
+    @Mock
+    RequestHeadersUriSpec rhus;
+    
+    @Mock
+    RequestHeadersSpec rhs;
+    
+    @Mock
+    ResponseSpec rs;
+    
+    @Mock
+    Mono<ApiExampleWrapper> apiExampleWrapperMono;
 
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	newsApi = new NewsApi();
+    	newsApi.setWebClient(webClient);
     }
 
     @Test
     void itShouldGetNewsStoryByTopic() {
         //given
+    	String topic = "cats";
+    	ApiExampleWrapper expected = new ApiExampleWrapper(); 
+    	
+    	when(webClient.get()).thenReturn(rhus);
+    	when(rhus.uri((Function<UriBuilder, URI>) any())).thenReturn(rhs);
+    	when(rhs.retrieve()).thenReturn(rs);
+    	when(rs.bodyToMono(ApiExampleWrapper.class)).thenReturn(apiExampleWrapperMono);
+    	when(apiExampleWrapperMono.block()).thenReturn(expected);
 
         //when
+    	ApiExampleWrapper actual = newsApi.getNewsStoryByTopic(topic);
 
         //then
+    	assertEquals(expected, actual);
     }
 
     @Test
     void itShouldFindStory(){
-        //given
+    	//given
+    	String topic = "cats";
+    	String title = "Cats Do Thing, Very Cool";
+    	String content = "Breaking News: Two cats did a thing. Politicians shocked!";
+    	String url = "www.catnews.com/articles/cats-do-thing-very-cool/";
+    	String expectedMessage = title + " -\n" + content + "\nFull article: " + url;
+    	
+    	ApiExampleWrapper wrapper = new ApiExampleWrapper();
+    	
+    	Article article = new Article();
+    	article.setTitle(title);
+    	article.setContent(content);
+    	article.setUrl(url);
+    	
+    	List<Article> articles = new ArrayList<Article>();
+    	articles.add(article);
+    	wrapper.setArticles(articles);
+    	
+    	when(webClient.get()).thenReturn(rhus);
+    	when(rhus.uri((Function<UriBuilder, URI>) any())).thenReturn(rhs);
+    	when(rhs.retrieve()).thenReturn(rs);
+    	when(rs.bodyToMono(ApiExampleWrapper.class)).thenReturn(apiExampleWrapperMono);
+    	when(apiExampleWrapperMono.block()).thenReturn(wrapper);
 
         //when
+    	String actualMessage = newsApi.findStory(topic);
 
         //then
+    	assertEquals(expectedMessage, actualMessage);
     }
 
 
--- src/test/java/_99_extra/_00_league_of_amazing_astronauts/LeagueOfAmazingAstronautsTest.java
+++ src/test/java/_99_extra/_00_league_of_amazing_astronauts/LeagueOfAmazingAstronautsTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 /*
@@ -18,45 +20,74 @@ When writing the tests, mock both the Rocketship and Astronaut for the sake of p
 class LeagueOfAmazingAstronautsTest {
 
     LeagueOfAmazingAstronauts underTest = new LeagueOfAmazingAstronauts();
+    
+    @Mock
+    Astronaut astronaut;
+    
+    @Mock
+    Rocketship rocketship;
 
     @BeforeEach
     void setUp() {
-
+    	MockitoAnnotations.openMocks(this);
+    	
+    	underTest = new LeagueOfAmazingAstronauts();
+    	underTest.setRocketship(rocketship);
     }
 
     @Test
     void itShouldPrepareAstronaut() {
         //given
-
+    	when(astronaut.isTrained()).thenReturn(true);
+    	
         //when
+    	underTest.prepareAstronaut(astronaut);
 
         //then
+    	verify(astronaut, times(1)).train();
+    	verify(rocketship, times(1)).loadOccupant(astronaut);
     }
 
     @Test
     void itShouldLaunchRocket() {
         //given
+    	String destination = "Mars";
+    	int milesToDestination = 68_000_000;
+    	
+    	when(rocketship.isLoaded()).thenReturn(true);
 
         //when
+    	underTest.launchRocket(destination);
 
         //then
+    	verify(rocketship, times(1)).setDestination(destination, milesToDestination);
+    	verify(rocketship, times(1)).launch();
     }
 
 
     @Test
     void itShouldThrowWhenDestinationIsUnknown() {
         //given
+    	String destination = "Uranus";
+    	
+    	when(rocketship.isLoaded()).thenReturn(true);
 
         //when
         //then
+    	Exception e = assertThrows(Exception.class, () -> underTest.launchRocket(destination));
+    	assertEquals("Destination is unavailable", e.getMessage());
     }
 
     @Test
     void itShouldThrowNotLoaded() {
         //given
+    	String destination = "Mars"; 
+    	
+    	when(rocketship.isLoaded()).thenReturn(false);
 
         //when
         //then
-
+    	Exception e = assertThrows(Exception.class, () -> underTest.launchRocket(destination));
+    	assertEquals("Rocketship is not loaded", e.getMessage());
     }
 }
